2ab9cedcaa48a1f61714d34400dd4cdcd4ceb004
7invincible7 <113597597+sambukalx@users.noreply.github.com>
2022-09-22 13:33:47 +0300

    Create Good.java

---
+++ Java labs/4th lab/ex 1(2 var)/Good.java
@@ -0,0 +1,21 @@
+public abstract class Good implements Priceable {
+    private double price;
+
+    public Good(double price) {
+        this.price = price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "price=" + price;
+    }
+}
