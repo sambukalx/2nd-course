69fe6d0ceecf4b0825ade88b5b4b0906afa848ad
7invincible7 <113597597+sambukalx@users.noreply.github.com>
2022-09-22 13:26:47 +0300

    Create FurnitureShopTest.java

---
+++ Java labs/3rd lab/ex 1(3 var)/FurnitureShopTest.java
@@ -0,0 +1,17 @@
+
+public class FurnitureShopTest {
+    public static void main(String[] args) {
+        FurnitureShop shop = new FurnitureShop(5);
+        shop.add(new Table(
+                "Металлический стол", 2, 1.3, 2, 2, 5930, "Металл+стекло", 0
+        ));
+        System.out.println(shop.getPrices());
+        System.out.println("Куплено: "+ shop.buy(3));
+        System.out.println(shop.getPrices());
+
+        for (Furniture f :
+                shop.getGoods()) {
+            f.jumpOn();
+        }
+    }
+}
