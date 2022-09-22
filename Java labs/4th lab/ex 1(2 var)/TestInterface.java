c378fd3ab6aedd59cd2bfa7f3369c2c20939f39e
7invincible7 <113597597+sambukalx@users.noreply.github.com>
2022-09-22 13:35:32 +0300

    Create TestInterface.java

---
+++ Java labs/4th lab/ex 1(2 var)/TestInterface.java
@@ -0,0 +1,12 @@
+public class TestInterface {
+    public static void main(String[] args) {
+        Priceable p = new Excursion("Путевка в Мексику", 55400, 1.205);
+        System.out.println(p);
+        System.out.println(p.getPrice());
+        p = new Laptop(12000, "Какая-то модель");
+        System.out.println(p);
+        p = new Mouth(800, 1800, "Черный");
+        System.out.println(p);
+    }
+
+}
