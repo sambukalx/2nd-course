ffd2fb12853e2323a6809aacbd8d923f7a7d7399
7invincible7 <113597597+sambukalx@users.noreply.github.com>
2022-09-22 13:34:38 +0300

    Create Mouth.java

---
+++ Java labs/4th lab/ex 1(2 var)/Mouth.java
@@ -0,0 +1,34 @@
+public class Mouth extends Good{
+    private int dpi;
+    private String color;
+
+    public Mouth(double price, int dpi, String color) {
+        super(price);
+        this.dpi = dpi;
+        this.color = color;
+    }
+
+    public int getDpi() {
+        return dpi;
+    }
+
+    public void setDpi(int dpi) {
+        this.dpi = dpi;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Mouth{" +
+                "dpi=" + dpi +
+                ", color='" + color + '\'' + ", " + super.toString() +
+                '}';
+    }
+}
